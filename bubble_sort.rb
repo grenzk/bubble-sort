24aa96bd94ef65a9138be3ccb712c402d6446398
grenzk <74033991+grenzk@users.noreply.github.com>
2022-03-18 00:10:40 +0800

    Add code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,19 @@
-require "pry-byebug"
+# frozen_string_literal: true
+
+require 'pry-byebug'
 
 def bubble_sort(numbers)
+  until numbers == numbers.sort
+    numbers.each_with_index do |number, index|
+      numbers[index] = number
+
+      if !numbers[index + 1].nil? && (numbers[index + 1] < numbers[index])
+        numbers[index], numbers[index + 1] =
+          numbers[index + 1], numbers[index]
+      end
+    end
+  end
+  numbers
 end
 
-puts bubble_sort(numbers)
+p bubble_sort([4, 3, 78, 2, 0, 2])
