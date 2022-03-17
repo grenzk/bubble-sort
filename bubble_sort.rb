c7fb8bf8b9a0f191bce411d7e55b3742a9d2ce85
grenzk <74033991+grenzk@users.noreply.github.com>
2022-03-18 07:44:24 +0800

    Refactor code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,17 +3,21 @@
 require 'pry-byebug'
 
 def bubble_sort(numbers)
-  until numbers == numbers.sort
+  until sorted?(numbers)
     numbers.each_with_index do |number, index|
       numbers[index] = number
 
       if !numbers[index + 1].nil? && (numbers[index + 1] < numbers[index])
-        numbers[index], numbers[index + 1] =
-          numbers[index + 1], numbers[index]
+        numbers[index], numbers[index + 1] = numbers[index + 1], numbers[index]
       end
     end
   end
   numbers
+end
+
+def sorted?(numbers)
+  numbers.reduce { |first, second| first <= second ? second : (return false) }
+  true
 end
 
 p bubble_sort([4, 3, 78, 2, 0, 2])
